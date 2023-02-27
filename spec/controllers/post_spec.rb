cad6f73a9ef0eae0ab0b8dfe762e6bef9088bef3
Mov305 <abd3052001@gmail.com>
2023-02-28 00:10:46 +0200

    Added all the tests and checked them

--- spec/controllers/post_spec.rb
+++ spec/controllers/post_spec.rb
@@ -1,1 +1,37 @@
-# testing the post controller
+require "rails_helper"
+
+RSpec.describe "Posts", type: :request do
+  describe "GET /posts #index" do
+    subject { get "/users/1/posts" }
+    before { subject }
+
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "has the correct placeholder text" do
+      expect(response.body).to include("posts")
+    end
+  end
+
+  describe "GET /posts #show" do
+    subject { get "/users/1/posts/1" }
+    before { subject }
+
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "has the correct placeholder text" do
+      expect(response.body).to include("post")
+    end
+  end
+end
--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -1,1 +1,40 @@
-# testing the user controller
+require "rails_helper"
+
+RSpec.describe "Users", type: :request do
+  describe "GET /users" do
+    # controller => users, action => index
+    subject { get "/users" }
+    before { subject }
+
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template(:index)
+    end
+
+    it "has the correct placeholder text" do
+      expect(response.body).to include("users")
+    end
+  end
+
+  describe "GET #show" do
+
+    # controller => users, action => show
+    subject { get "/users/1" }
+    before { subject }
+
+    it "responds successfully with an HTTP 200 status code" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      expect(response).to render_template(:show)
+    end
+
+    it "has the correct placeholder text" do
+      expect(response.body).to include("user")
+    end
+  end
+end
