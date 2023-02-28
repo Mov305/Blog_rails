704e65744c18d14a46b72a347197cab8b83f76d5
Mov305 <abd3052001@gmail.com>
2023-02-28 18:58:43 +0200

    Fixed the test environment

--- spec/controllers/post_spec.rb
+++ spec/controllers/post_spec.rb
@@ -1,37 +1,37 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts #index' do
-    subject { get '/users/1/posts' }
+RSpec.describe "Posts", type: :request do
+  describe "GET /posts #index" do
+    subject { get "/users/:user_id/posts" }
     before { subject }
 
-    it 'responds successfully with an HTTP 200 status code' do
+    it "responds successfully with an HTTP 200 status code" do
       expect(response).to have_http_status(:success)
     end
 
-    it 'renders the index template' do
+    it "renders the index template" do
       expect(response).to render_template(:index)
     end
 
-    it 'has the correct placeholder text' do
-      expect(response.body).to include('posts')
+    it "has the correct placeholder text" do
+      expect(response.body).to include("posts")
     end
   end
 
-  describe 'GET /posts #show' do
-    subject { get '/users/1/posts/1' }
+  describe "GET /posts #show" do
+    subject { get "/users/:user_id/posts/:post_id" }
     before { subject }
 
-    it 'responds successfully with an HTTP 200 status code' do
+    it "responds successfully with an HTTP 200 status code" do
       expect(response).to have_http_status(:success)
     end
 
-    it 'renders the show template' do
+    it "renders the show template" do
       expect(response).to render_template(:show)
     end
 
-    it 'has the correct placeholder text' do
-      expect(response.body).to include('post')
+    it "has the correct placeholder text" do
+      expect(response.body).to include("post")
     end
   end
 end
--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -1,39 +1,39 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /users' do
+RSpec.describe "Users", type: :request do
+  describe "GET /users" do
     # controller => users, action => index
-    subject { get '/users' }
+    subject { get "/users" }
     before { subject }
 
-    it 'responds successfully with an HTTP 200 status code' do
+    it "responds successfully with an HTTP 200 status code" do
       expect(response).to have_http_status(:success)
     end
 
-    it 'renders the index template' do
+    it "renders the index template" do
       expect(response).to render_template(:index)
     end
 
-    it 'has the correct placeholder text' do
-      expect(response.body).to include('users')
+    it "has the correct placeholder text" do
+      expect(response.body).to include("users")
     end
   end
 
-  describe 'GET #show' do
+  describe "GET #show" do
     # controller => users, action => show
-    subject { get '/users/1' }
+    subject { get "/users/:user_id" }
     before { subject }
 
-    it 'responds successfully with an HTTP 200 status code' do
+    it "responds successfully with an HTTP 200 status code" do
       expect(response).to have_http_status(:success)
     end
 
-    it 'renders the show template' do
+    it "renders the show template" do
       expect(response).to render_template(:show)
     end
 
-    it 'has the correct placeholder text' do
-      expect(response.body).to include('user')
+    it "has the correct placeholder text" do
+      expect(response.body).to include("user")
     end
   end
 end
