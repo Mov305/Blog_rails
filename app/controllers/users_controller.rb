9c3dee1ef68eada76ec63e5b37bdebbc2c7ba3e1
Mov305 <abd3052001@gmail.com>
2023-03-02 00:17:49 +0200

    Added the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    # @users = User.all
+    @users = User.all
   end
 
   def show
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @latest = @user.return_recent_posts
   end
 end
