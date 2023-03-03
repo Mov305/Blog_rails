13af7fd45df4ff7897035e29890d9889736f3c47
Mov305 <abd3052001@gmail.com>
2023-03-04 00:19:39 +0200

    Fixed the linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   # make a current_user method available to all controllers
   def current_user
     User.first
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,15 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new()
+    @like = Like.new
     @like.user_id = current_user.id
     @like.post_id = params[:post_id]
     # catch the error if the like could not be saved
     if @like.valid?
       @like.save
-      redirect_to user_post_path(current_user.id, params[:post_id])
     else
-      flash[:alert] = "You have already liked this post"
-      redirect_to user_post_path(current_user.id, params[:post_id])
+      flash[:alert] = 'You have already liked this post'
     end
+    redirect_to user_post_path(current_user.id, params[:post_id])
   end
 
   def destroy
